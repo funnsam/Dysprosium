ad718a326000666f557b4c80688bcc46168fbcd9
funnsam <funnsam8@gmail.com>
2025-03-14 20:55:18 +0800

    idk

--- src/eval/weight.rs
+++ src/eval/weight.rs
@@ -1,6 +1,8 @@
 #[cfg(feature = "eval-track")]
 use core::cell::RefCell;
-use std::ops::Mul;
+use std::ops::{Add, AddAssign, Mul, MulAssign};
+
+use arrayvec::ArrayVec;
 
 use super::Eval;
 
@@ -8,9 +10,7 @@ use super::Eval;
 pub struct Tracker<'a> {
     pub value: i16,
     #[cfg(feature = "eval-track")]
-    pub mul: f64,
-    #[cfg(feature = "eval-track")]
-    pub track: &'a RefCell<TrackerNode>,
+    pub track: ArrayVec<(&'a RefCell<TrackerNode>, f64), 256>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, PartialOrd)]
@@ -42,22 +42,41 @@ impl Weight {
     }
 }
 
-impl Tracker<'_> {}
+impl Tracker<'_> {
+    pub fn finalize() {
+        todo!();
+    }
+}
 
-impl<'a> Mul<&'a Weight> for Eval {
-    type Output = Tracker<'a>;
+impl<'a> From<&'a Weight> for Tracker<'a> {
+    fn from(value: &'a Weight) -> Self {
+        #[cfg(feature = "eval-track")]
+        let mut track = ArrayVec::new_const();
+        #[cfg(feature = "eval-track")]
+        track.push((&value.track, 1.0));
 
-    fn mul(self, rhs: &'a Weight) -> Self::Output {
-        // #[cfg(feature = "eval-track")] {
-        //     let mut track = rhs.track.borrow_mut();
-        //     track.derivative += val as f64;
-        //     track.frequency += 1;
-        // }
+        Self {
+            value: value.value,
+            #[cfg(feature = "eval-track")]
+            track,
+        }
+    }
+}
 
-        Tracker {
-            value: self.0 * rhs.value,
-            mul: 1.0,
-            track: &rhs.track,
+impl<'a> AddAssign<Tracker<'a>> for Tracker<'a> {
+    fn add_assign(&mut self, rhs: Tracker<'a>) {
+        self.value += rhs.value;
+        #[cfg(feature = "eval-track")]
+        self.track.extend(rhs.track);
+    }
+}
+
+impl<'a> MulAssign<i16> for Tracker<'a> {
+    fn mul_assign(&mut self, rhs: i16) {
+        self.value *= rhs;
+
+        for i in self.track.iter_mut() {
+            i.1 *= rhs as f64;
         }
     }
 }
