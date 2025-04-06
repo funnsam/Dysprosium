8e5035c5f3397737ef9ff2ba32818faaa797b901
funnsam <funnsam8@gmail.com>
2025-04-06 02:30:22 +0000

    chore: add feature for moveord hash move
    bench: 3346733

--- src/move_order.rs
+++ src/move_order.rs
@@ -73,7 +73,7 @@ impl<const MAIN: bool> crate::SmpThread<'_, MAIN> {
         killer: &KillerTable,
     ) -> i32 {
 
-        if tte.is_some_and(|tte| tte.next == m) {
+        if cfg!(feature = "moveord-hash") && tte.is_some_and(|tte| tte.next == m) {
             i32::MAX
         } else if cfg!(feature = "moveord-mvv") && game.is_capture(m) {
             mvv_lva(game, m) as i32 * 327601
