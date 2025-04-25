c34d9f808b26cab810da06e57318a4cee71223b3
funnsam <funnsam8@gmail.com>
2025-04-25 20:48:16 +0800

    refac: rewrite shared table with safe rust
    bench: 3983897

--- src/shared_table.rs
+++ src/shared_table.rs
@@ -1,14 +1,14 @@
-use core::cell::UnsafeCell;
+use core::cell::Cell;
 use bytemuck::*;
 use fxhash::hash64;
 
-pub struct SharedHashTable<T: Clone + Sized + Send + Sync + NoUninit> {
-    inner: Box<[UnsafeCell<TableEntry<T>>]>,
+pub struct SharedHashTable<T: Clone + Copy + Sized + Send + Sync + NoUninit> {
+    inner: Box<[Cell<TableEntry<T>>]>,
 }
 
 #[repr(packed)]
-#[derive(Default, Clone)]
-pub struct TableEntry<T: Clone + Sized + Send + Sync + NoUninit> {
+#[derive(Default, Clone, Copy)]
+pub struct TableEntry<T: Clone + Copy + Sized + Send + Sync + NoUninit> {
     key: u64,
     hash: u64,
     value: T,
@@ -21,32 +21,32 @@ impl<T: Default + Clone + Sized + Send + Sync + NoUninit> SharedHashTable<T> {
 
     pub fn new(size: usize) -> Self {
         let mut inner = vec![];
-        inner.resize_with(size, UnsafeCell::default);
+        inner.resize_with(size, Cell::default);
 
         Self { inner: inner.into() }
     }
 
     pub fn clear(&mut self) {
         for e in self.inner.iter_mut() {
-            unsafe { (*e.get()).hash = 0 };
+            e.set(TableEntry::default());
         }
     }
 
     pub fn insert(&self, key: u64, value: T) {
         let hash = hash64(&(key, bytemuck::bytes_of(&value)));
         let entry = TableEntry { key, hash, value };
-        unsafe { *self.inner[key as usize % self.inner.len()].get() = entry; }
+        self.inner[key as usize % self.inner.len()].set(entry);
     }
 
     pub fn get(&self, key: u64) -> Option<T> {
-        let entry = unsafe { (*self.inner[key as usize % self.inner.len()].get()).clone() };
+        let entry = self.inner[key as usize % self.inner.len()].get();
         let value = entry.value;
 
         (entry.key == key && entry.hash == hash64(&(key, bytemuck::bytes_of(&value)))).then_some(value)
     }
 
     pub fn get_place(&self, key: u64) -> Option<T> {
-        let entry = unsafe { (*self.inner[key as usize % self.inner.len()].get()).clone() };
+        let entry = self.inner[key as usize % self.inner.len()].get();
         let value = entry.value;
 
         (entry.hash == hash64(&(key, bytemuck::bytes_of(&value)))).then_some(value)
@@ -54,7 +54,7 @@ impl<T: Default + Clone + Sized + Send + Sync + NoUninit> SharedHashTable<T> {
 
     pub fn filter_count<F: Fn(T) -> bool>(&self, filter: F) -> usize {
         self.inner.iter().filter(|entry| {
-            let entry = unsafe { (*entry.get()).clone() };
+            let entry = entry.get();
             let value = entry.value;
 
             entry.hash == hash64(&bytemuck::bytes_of(&value)) && filter(value)
